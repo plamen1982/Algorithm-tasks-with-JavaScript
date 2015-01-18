16ec91383d6bce1c8264e29a100bfe9c449d4b30
plamen1982 <plamen.hristov@ymail.com>
2015-01-17 23:57:24 -0500

    New Approach for Sum Of Two Huge Numbers

--- 4. MorePlainJS/08SumOfTwoHugeNumbers/sumTwoHugeNumbers.js
+++ 4. MorePlainJS/08SumOfTwoHugeNumbers/sumTwoHugeNumbers.js
@@ -1,127 +1,44 @@
-function caseOne(arr){
-	var i;
-	var result;
-	var resultStr = '';
-	var remainder1 = 0;
-	var remainder2 = 0;
-	var currentSum = 0;
-	var arrResult = [];
-	var num1 = parseInt(arr[0]);
-	var num2 = parseInt(arr[1]);
-	while(num1 > 1){
-		//reset them later
-		remainder1 = num1 % 10;
-		remainder2 = num2 % 10;
-		num1 = Math.floor(num1 / 10);
-		num2 = Math.floor(num2 / 10);
-		currentSum = remainder1 + remainder2;
-		if( currentSum >= 10){
-			resultStr += currentSum % 10;
-			num1 ++;
-		}
-		else {
-			resultStr += currentSum;
-		}
-		currentSum = 0;
-		remainder1 = 0;
-		remainder2 = 0;
-
-	}
-	result = resultStr.split('').reverse().join('');
-	console.log(result);
-
-}
-function caseTwo(arr){
-	var i;
-	var result;
-	var resultStr = '';
-	var remainder1 = 0;
-	var remainder2 = 0;
-	var currentSum = 0;
-	var arrResult = [];
-	var num1 = parseInt(arr[0]);
-	var num2 = parseInt(arr[1]);
-	while(num1 > 1){
-		//reset them later
-		remainder1 = num1 % 10;
-		remainder2 = num2 % 10;
-		num1 = Math.floor(num1 / 10);
-		num2 = Math.floor(num2 / 10);
-		currentSum = remainder1 + remainder2;
-		if( currentSum >= 10){
-			resultStr += currentSum % 10;
-			num1 ++;
-		}
-		else if( arr[0][0] !== 0){
-			resultStr += currentSum;
-		}
-		else {
-			resultStr += currentSum;
-		}
-		currentSum = 0;
-		remainder1 = 0;
-		remainder2 = 0;
-
-	}
-	result = resultStr.split('').reverse().join('');
-	console.log(result);
-
-}
-function caseThree(arr){
-	var i;
-	var result;
-	var resultStr = '';
-	var remainder1 = 0;
-	var remainder2 = 0;
-	var currentSum = 0;
-	var arrResult = [];
-	var num1 = parseInt(arr[0]);
-	var num2 = parseInt(arr[1]);
-	while(num2 > 1){
-		//reset them later
-		remainder1 = num1 % 10;
-		remainder2 = num2 % 10;
-		num1 = Math.floor(num1 / 10);
-		num2 = Math.floor(num2 / 10);
-		currentSum = remainder1 + remainder2;
-		if( currentSum >= 10){
-			resultStr += currentSum % 10;
-			num2 ++;
-		}
-		else if( arr[1][0] !== 0){
-			resultStr += currentSum;
-		}
-		else {
-			resultStr += currentSum;
-		}
-		currentSum = 0;
-		remainder1 = 0;
-		remainder2 = 0;
-
-	}
-	result = resultStr.split('').reverse().join('');
-	console.log(result);
-
-}
-function findTheCase(str1, str2){
-	if(str1.length === str2.length){
-		return 1;
-	}
-	if(str1.length > str2.length){
-		return 2;
-	}
-	if(str1.length < str2.length){
-		return 3;
-	}
-}
-
 function sumTwoHugeNumbers(arr){
-	switch(findTheCase(arr[0],arr[1])){
-		case 1 : caseOne(arr);
-			break;
-		case 2 : caseTwo(arr);
-			break;
-		case 3: caseThree(arr);
-			break;
+		var i;
+		var j;
+		var k;
+		var fillingString = '';
+		var remainder = '';
+		var result = [];
+		var diffLength = 0;
+		var calc = 0;
+		var remainderOfCalc = 0;
+		var firstEl = arr[0];
+		var secondEl = arr[1];
+		var firstElLength = arr[0].length;
+		var secondElLength = arr[1].length;
+
+		if(firstElLength >= secondElLength){
+			diffLength = firstElLength - secondElLength;
+				for(i = 0; i < diffLength; i++){
+		 		fillingString += 0;
+			}
+			secondEl = fillingString.concat(secondEl)
+			for(j = firstElLength - 1, k = 1; j > 0, k < firstElLength; j--, k++){
+
+				// Calculating the last elements of the strings, and push it into an array
+				calc = parseInt(firstEl[firstElLength - k]) + parseInt(secondEl[firstElLength - k]);
+				remainderOfCalc = calc % 10;
+				result.push(remainderOfCalc)
+				// Reset calc, remainderOfCals
+				if(calc > 10){
+					firstEl = firstEl.split('');
+					firstEl[firstElLength - k - 2] = parseInt(firstEl[firstElLength - k - 2]) + 1;
+					firstEl = firstEl.join('');
+				}
+				calc = 0;
+			}
+			console.log(result.reverse().join(''))
+		}
+		else {
+			diffLength = secondElLength - firstElLength;
+				for(i = 0; i < diffLength; i++){
+		 			fillingString += 0;
+		}
 	}
 }
