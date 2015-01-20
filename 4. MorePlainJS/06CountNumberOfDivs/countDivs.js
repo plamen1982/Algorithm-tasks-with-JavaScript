9801371f6a4973f6b0a2e0ff5efa91cc6fa815df
plamen1982 <plamen.hristov@ymail.com>
2015-01-20 00:59:54 -0500

    New approach for Problem N 4

--- 4. MorePlainJS/06CountNumberOfDivs/countDivs.js
+++ 4. MorePlainJS/06CountNumberOfDivs/countDivs.js
@@ -1,3 +1,14 @@
-function countDivs(html){
-	console.log(document.getElementsByTagName('div').length)
+function countDivs(str){
+	var index = 0;
+	var count = 0;
+	var result = document.getElementById('result')
+	while(index > -1){
+		index = str.indexOf('<div')
+		str = str.slice(index + 5)
+		if(index > -1){
+			count ++;
+		}
+		
+	}
+	result.innerHTML = 'The numbers of the divs are: ' + count;
 }
