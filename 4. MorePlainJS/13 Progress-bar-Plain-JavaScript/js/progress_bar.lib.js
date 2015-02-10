e9184075e034188a2cedc4e7ee90e76b3e79b384
plamen1982 <plamen.hristov@ymail.com>
2015-02-10 17:09:22 -0500

    Progress bar with plain javascript

---
+++ 4. MorePlainJS/13 Progress-bar-Plain-JavaScript/js/progress_bar.lib.js
@@ -0,0 +1,28 @@
+function progress_bar(container){
+
+	var bar = document.createElement('div');
+	var text = document.createElement('div');
+
+	bar.style.width = '0px';
+	bar.style.height = '100%';
+	bar.style.background = '#eef';
+
+	container.appendChild(bar);
+
+	text.style.marginTop = '-26px';
+	text.style.textAlign = 'center';
+	text.style.color = '#111';
+	text.style.textSize = '15px';
+
+	text.appendChild(document.createTextNode('0%'))
+
+	container.appendChild(text);
+
+	this.set_percentage = function(percantage){
+		bar.style.width = percantage + '%';
+
+		text.removeChild(text.firstChild);
+		text.appendChild(document.createTextNode(percantage + '%'))
+	}
+
+}
