b5ed4d7a029b9ce242a8999ac5dcec21e6d5d3c1
plamen1982 <plamen.hristov@ymail.com>
2015-03-04 22:20:55 -0500

    Fixed Daggers and Swords

--- 4. MorePlainJS/14.Daggers and Swords/main.js
+++ 4. MorePlainJS/14.Daggers and Swords/main.js
@@ -1,49 +1,50 @@
-function solve(input) {
-	var arrayOfStrings = input.map(Number)
-	var arrayOfInt = [];
+function solve(arrayOfStrings) {
+
 	var i;
 	var j;
 	var k;
 	var result;
-	var typeBlade = "";
+	var typeBlade = '';
+	var arrayOfInt = [];
+
 	result = '<table border="1">' + "\n" +
-	"<thead>"+"\n"+'<tr><th colspan="3">Blades</th></tr>' + "\n" +
-	"<tr><th>Length [cm]</th><th>Type</th><th>Application</th></tr>" + "\n" +
-	"</thead>" + "\n" +
-	"<tbody>" + "\n";
+	'<thead>'+'\n'+'<tr><th colspan="3">Blades</th></tr>' + '\n' +
+	'<tr><th>Length [cm]</th><th>Type</th><th>Application</th></tr>' + '\n' +
+	'</thead>' + '\n' +
+	'<tbody>' + '\n';
 	for (i = 0; i < arrayOfStrings.length; i++) {
 		arrayOfInt[i] = parseInt(arrayOfStrings[i]);
 	}
 
 	for (j = 0; j < arrayOfInt.length; j++) {
-		if (arrayOfInt[j] > 3) {
+		if (arrayOfInt[j] > 10) {
 			if (arrayOfInt[j] > 40) {
-				typeBlade = "sword";
-				result += "<tr><td>" + arrayOfInt[j] + "</td><td>" + typeBlade + "</td><td>" + nameOfTheBlade(arrayOfInt[j]) + "</td></tr>" + "\n";
-				typeBlade = "";
+				typeBlade = 'sword';
+				result += '<tr><td>' + arrayOfInt[j] + '</td><td>' + typeBlade + '</td><td>' + nameOfTheBlade(arrayOfInt[j]) + '</td></tr>' + '\n';
+
 			}
 			else {
-				typeBlade = "dagger";
-				result += "<tr><td>" + arrayOfInt[j] + "</td><td>" + typeBlade + "</td><td>" + nameOfTheBlade(arrayOfInt[j]) + "</td></tr>" + "\n";
-				typeBlade = "";
+				typeBlade = 'dagger';
+				result += '<tr><td>' + arrayOfInt[j] + '</td><td>' + typeBlade + '</td><td>' + nameOfTheBlade(arrayOfInt[j]) + '</td></tr>' + '\n';
+
 			}
 		}
 	}
-	result +=  "</tbody>" +"\n"+
-	"</table>";
+	result +=  '</tbody>' +'\n'+
+	'</table>';
 	console.log(result);
 	function nameOfTheBlade(integer) {
 		switch (integer % 5) {
 			case 0:
-				return "*rap-poker";
+				return '*rap-poker';
 			case 1:
-				return "blade";
+				return 'blade';
 			case 2:
-				return "quite a blade";
+				return 'quite a blade';
 			case 3:
-				return "pants-scraper";
+				return 'pants-scraper';
 			case 4:
-				return "frog-butcher";
+				return 'frog-butcher';
 		}
 	}
 }
@@ -52,5 +53,5 @@ solve(
 		"19.4",
 		"13",
 		"55.8",
-		"126.96541651",
-		"3"])
+		"999.96541651",
+		"3"]);
