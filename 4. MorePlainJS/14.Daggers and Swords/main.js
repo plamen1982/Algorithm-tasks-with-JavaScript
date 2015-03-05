9db18a98c581dffd9f43b4056e02b6d61028d45c
plamen1982 <plamen.hristov@ymail.com>
2015-03-04 21:44:22 -0500

    Daggers and Swords

---
+++ 4. MorePlainJS/14.Daggers and Swords/main.js
@@ -0,0 +1,56 @@
+function solve(input) {
+	var arrayOfStrings = input.map(Number)
+	var arrayOfInt = [];
+	var i;
+	var j;
+	var k;
+	var result;
+	var typeBlade = "";
+	result = '<table border="1">' + "\n" +
+	"<thead>"+"\n"+'<tr><th colspan="3">Blades</th></tr>' + "\n" +
+	"<tr><th>Length [cm]</th><th>Type</th><th>Application</th></tr>" + "\n" +
+	"</thead>" + "\n" +
+	"<tbody>" + "\n";
+	for (i = 0; i < arrayOfStrings.length; i++) {
+		arrayOfInt[i] = parseInt(arrayOfStrings[i]);
+	}
+
+	for (j = 0; j < arrayOfInt.length; j++) {
+		if (arrayOfInt[j] > 3) {
+			if (arrayOfInt[j] > 40) {
+				typeBlade = "sword";
+				result += "<tr><td>" + arrayOfInt[j] + "</td><td>" + typeBlade + "</td><td>" + nameOfTheBlade(arrayOfInt[j]) + "</td></tr>" + "\n";
+				typeBlade = "";
+			}
+			else {
+				typeBlade = "dagger";
+				result += "<tr><td>" + arrayOfInt[j] + "</td><td>" + typeBlade + "</td><td>" + nameOfTheBlade(arrayOfInt[j]) + "</td></tr>" + "\n";
+				typeBlade = "";
+			}
+		}
+	}
+	result +=  "</tbody>" +"\n"+
+	"</table>";
+	console.log(result);
+	function nameOfTheBlade(integer) {
+		switch (integer % 5) {
+			case 0:
+				return "*rap-poker";
+			case 1:
+				return "blade";
+			case 2:
+				return "quite a blade";
+			case 3:
+				return "pants-scraper";
+			case 4:
+				return "frog-butcher";
+		}
+	}
+}
+solve(
+	["17.8",
+		"19.4",
+		"13",
+		"55.8",
+		"126.96541651",
+		"3"])
